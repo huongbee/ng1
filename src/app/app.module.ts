15f42c4cf78fdf32ad3b61597bd1a739358a4a7b
Huong <you@example.com>
2018-01-26 14:29:27 +0700

    install-boostrap-jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component'
@@ -11,7 +12,9 @@ import { HeroesComponent } from './heroes/heroes.component'
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
